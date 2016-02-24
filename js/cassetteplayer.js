d1450cbbb028963a85756bbec2408c7d902d1e96
Carlos Tenorio <basstobass94@gmail.com>
2016-02-24 12:16:01 +0100

    Buttons Functional and boostrap applied

--- js/cassetteplayer.js
+++ js/cassetteplayer.js
@@ -1,36 +1,32 @@
 /************************************/
 //HIDDEN BIG CASSETE AND DIV BLACK
 /************************************/
-  $(document).ready(function () { 
+$(document).ready(function () { 
     $(document).click(function() {
       $("#divBlack").click(function(){        
           $('#divCassette').css('visibility', 'hidden');
           $('#divBlack').css('visibility', 'hidden');
+          $('#divInfoRight').css('visibility', 'hidden');
       });
-    });
   });
+});
 /********************************/
 //SHOW BIG CASSETE AND DIV BLACK
 /********************************/
 
+
 function blackFunction() {
     document.getElementById("divCassette").style.visibility="visible";
+    document.getElementById("divInfoRight").style.visibility="visible";
     document.getElementById("divBlack").style.visibility="visible";
+    //scroll the document to top    
+    window.scrollTo(0, 0);
 }
-
-
-
-
-
-
-
-
-
 
 
 $(document).ready(function(e) {
     var mozillaPresente = false,
-        mozilla = (function detectarNavegador(navegador) {
+    mozilla = (function detectarNavegador(navegador) {
         if(navegador.indexOf("Firefox") != -1 ) {
             mozillaPresente = true;
         }   
@@ -40,9 +36,9 @@ $(document).ready(function(e) {
         el = $('.cajainterna');
         el.addClass(efecto);
         el.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
-        function (e) {
-            el.removeClass(efecto);
-        });
+            function (e) {
+                el.removeClass(efecto);
+            });
     }
     function mostrar(e) {
         $(".cajaexterna").show();
@@ -52,10 +48,10 @@ $(document).ready(function(e) {
     function ocultar() {
         $(".cajaexterna").fadeOut("fast", function() {
             if(mozillaPresente) {
-            setTimeout(function() {
-                $(".cajainterna").removeClass("bounceIn");
-            }, 5);
-        }
+                setTimeout(function() {
+                    $(".cajainterna").removeClass("bounceIn");
+                }, 5);
+            }
         });         
     }
 
@@ -63,6 +59,37 @@ $(document).ready(function(e) {
     $("a.mostrarmodal").click(mostrar);
     $("a.cerrarmodal").click(ocultar);
 }); 
+
+
+/********************************/
+//PROGRESS BAR 
+/********************************/
+/*
+jQuery(function ($) {
+    $('#progressBar').click(function () {
+        var val = Math.floor((Math.random() * 100)) + '%';
+        $('.progress-bar').width(val).text(val)
+    })
+});
+*/
+
+//document.getElementById('progressBar').addEventListener('click', function (e) {
+
+
+function progressBar() {
+    var e = document.getElementById("progressBar");
+    var x = e.pageX - this.offsetLeft, // or e.offsetX (less support, though)
+
+    //alert("holaaa");
+    y = e.pageY - this.offsetTop,  // or e.offsetY
+    clickedValue = x * this.max / this.offsetWidth,
+    isClicked = clickedValue <= this.value;
+    
+    if (isClicked) {
+        alert('You clicked within the value range at: ' + clickedValue);
+    }
+}
+//});
 
 
 
